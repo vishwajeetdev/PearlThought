919302db0eed2f822dc57b92370b4cefb108f9fd
Vishwajeet <vishwajeet.singh@mayadataprivacy.eu>
2024-08-25 01:07:34 +0530

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { EmailService } from './EmailService';
 import { MockEmailProvider } from './MockEmailProvider';
 
-// Create instances of the mock email providers
+
 const provider1 = new MockEmailProvider('Provider1');
 const provider2 = new MockEmailProvider('Provider2');
 
-// Instantiate the EmailService with the mock providers
 const emailService = new EmailService([provider1, provider2]);
 
-// Array of email addresses
 const emailAddresses = [
     'vishwajeet@gmail.com',
     'vishwajeet.singh@yahoo.com',
@@ -19,7 +17,6 @@ const emailAddresses = [
 const subject = 'Pearl Thought';
 const body = 'Working on this mini project for Internship at Pearl Thought';
 
-// Function to test the email sending service
 async function testEmailService() {
     for (const email of emailAddresses) {
         try {
@@ -31,5 +28,4 @@ async function testEmailService() {
     }
 }
 
-// Run the test function
 testEmailService();
